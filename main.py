5ddfcde28ad13a685375c1d78e40a81c914e62a5
togathrust <togathruster@gmail.com>
2024-09-05 11:06:42 +0800

    Created different classes for different uses.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import sys
 import threading
 from datetime import datetime
+from typing import Type
 
 # standard libraries
 import pandas as pd
@@ -25,20 +26,192 @@ ctk.set_default_color_theme("theme.json") # todo create custom theme, also maybe
 # Other files
 import global_var
 
-class GUI:
-    def __init__(self):
-        # libraries
+class ErrorHandler:
+    def __init__(self, gui):
         self.error_messages = global_var.error_messages
-        self.UI_grid = global_var.UI_grid
-        # dataframes
+        self.gui = gui
+
+    def raise_question_box(self, error_type: str) -> str:
+        user_selection = messagebox.askquestion(title=self.error_messages[error_type][0],
+                                                message=self.error_messages[error_type][1],
+                                                icon=messagebox.WARNING, type=messagebox.OKCANCEL)
+        return user_selection
+
+    def raise_error_box(self, error_type: str, log_str: str | None) -> None:
+        messagebox.showerror(title=self.error_messages[error_type][0],
+                             message=self.error_messages[error_type][1])
+        if log_str is None:
+            log_str = self.error_messages[error_type][1]
+        self.gui.logger(log_str=log_str)
+        return None
+
+class FileHandler:
+    def __init__(self, gui):
         self.df_raw = None
         self.df_cleaned = None
-        # threading
+        self.gui = gui
+
+    def validate_file_path(self, file_path: str, have_headers: bool | int) -> None:
+        if not os.path.exists(path=file_path):
+            error_handler.raise_error_box(error_type="Invalid File Path", log_str=f"Invalid File Path: '{file_path}'")
+            self.gui.run_button.configure(state=ctk.DISABLED)
+            return None
+        try:
+            header = None
+            if have_headers:
+                header = 'infer'
+            df_validated = pd.read_csv(file_path, dtype=str, header=header, encoding='utf-8')
+        except UnicodeDecodeError:
+            error_handler.raise_question_box(error_type="UnicodeDecodeError")
+            self.gui.run_button.configure(state=ctk.DISABLED)
+            return None
+        self.gui.logger(f"File path: '{file_path}'")
+        self.gui.run_button.configure(state=ctk.NORMAL)
+        self.gui.entry_box.configure(state=ctk.DISABLED)
+        self.df_raw = df_validated.copy()
+        return None
+
+    def clean_raw_df(self) -> Type[AttributeError] | None:
+        # df_to_clean = pd.DataFrame()
+        try:  # Raise exception when df is empty.
+            df_to_clean = self.df_raw.apply(lambda col: col.dropna().reset_index(drop=True))
+            self.gui.logger(f"Removed NaN rows.")
+            max_len = df_to_clean.apply(len).max()
+            df_to_clean = df_to_clean.apply(lambda col: col.reindex(range(max_len)))
+        except AttributeError:
+            error_handler.raise_error_box(error_type="AttributeError", log_str=None)
+            return AttributeError
+        if self.gui.have_name_and_code.get():
+            df_to_clean = combinations_generator.combine_columns(df_to_clean)
+        self.df_cleaned = df_to_clean.replace('%%', np.nan, regex=False)
+        return None
+
+    @staticmethod
+    def get_new_file_path(entry_box_path: str) -> str:
+        temp_path_list = entry_box_path.split(".")
+        new_file_path = temp_path_list[0] + "_output" + ".csv"
+        while os.path.exists(new_file_path):  # fixme file names checked for duplicates not very profound
+            temp_path_list = new_file_path.split(".")
+            new_file_path = temp_path_list[0] + "(1)" + ".csv"
+        return new_file_path
+
+class CombinationsGenerator:
+    def __init__(self, gui):
+        self.gui = gui
         self.process_thread = None
         self.stop_event = threading.Event()
         self.expected_output = 0
         self.total_processed = 0
-        # logging
+        self.write_path = ""
+
+    @staticmethod
+    def get_expected_output(df: pd.DataFrame) -> int:
+        number_in_column = df.notna().sum()
+        expected_combinations = number_in_column.prod()
+        return expected_combinations
+
+    # Raise exception when data to process is too large.
+    def limit_check(self, df: pd.DataFrame, warning_limit: int = 1_000_000) -> tuple[int, str | None]:
+        expected_outputs = self.get_expected_output(df)
+        if expected_outputs > warning_limit:
+            return expected_outputs, error_handler.raise_question_box(error_type="LimitWarning")
+        return expected_outputs, None
+
+    @ staticmethod
+    def combine_columns(df: pd.DataFrame) -> pd.DataFrame:
+        concatenated_columns = []
+        for i in range(0, len(df.columns), 2):
+            # Concatenate two columns
+            if i + 1 < len(df.columns):
+                combined = df.iloc[:, i].fillna('') + '%%' + df.iloc[:, i + 1].fillna('')
+            else:
+                combined = df.iloc[:, i].fillna('')  # Handle odd number of columns
+            concatenated_columns.append(combined)
+        df_combined = pd.concat(concatenated_columns, axis=1)
+        return df_combined
+
+    @ staticmethod
+    def split_columns(df: pd.DataFrame) -> pd.DataFrame | AttributeError:
+        df_split = pd.DataFrame()
+        try:
+            for col in df.columns:
+                split_cols = df[col].str.split('%%', expand=True)
+                split_cols.columns = [f"{col}_{i + 1}" for i in range(split_cols.shape[1])]
+                df_split = pd.concat([df_split, split_cols], axis=1)
+        except AttributeError:
+            return AttributeError
+        return df_split
+
+    @ staticmethod
+    def chunk_generator(df: pd.DataFrame, chunk_size: int) -> list | None:
+        generator = itertools.product(*[df.df_cleaned[col] for col in df.df_cleaned.columns])
+        chunk = []
+        for i, combination in enumerate(generator):
+            chunk.append(combination)
+            if len(chunk) == chunk_size:
+                yield chunk
+                chunk = []
+        if chunk:
+            yield chunk
+        return None
+
+    def write_chunk(self, chunk: list, file_path: str, write_header: bool) -> None | str:
+        df_output = pd.DataFrame(chunk, columns=file_handler.df_cleaned.columns)
+        df_output.dropna(inplace=True)
+        if df_output.empty:
+            return
+        if self.gui.have_name_and_code.get():
+            df_output = CombinationsGenerator.split_columns(df_output)
+            if df_output == AttributeError:
+                return "RuntimeError"
+            df_output.columns = file_handler.df_raw.columns
+        # fixme chinese char breaks when writing from df to csv
+        file_lock = threading.Lock()
+        with file_lock:
+            df_output.to_csv(file_path, index=False, header=write_header, mode='a')
+        self.total_processed += df_output.shape[0]
+        percent_complete = round((self.total_processed / self.expected_output) * 100, 2)
+        self.gui.log_label.configure(text=f"Loading... {percent_complete}% complete.")
+        if percent_complete == 100: # stop the main process cleanly
+            self.stop_event.set()
+            if self.process_thread.is_alive():
+                self.process_thread.join()
+        return
+
+    # fixme find out where the extra data coming from, and how to exit the main program when finished.
+    def thread_handler(self, df: pd.DataFrame , chunk_size: int = 100_000) -> None:
+        self.gui.logger(log_str=f"Process to create new combinations has started.")
+        self.total_processed = 0
+        max_threads = 4
+        threads = []
+        for i, chunk in enumerate(self.chunk_generator(df, chunk_size=chunk_size)):
+            print(f"chunk size: {len(chunk)}") # todo remove debug line
+            write_header = (i == 0)
+            thread = threading.Thread(target=self.write_chunk, args=(chunk, write_header), daemon=True)
+            threads.append(thread)
+            self.stop_event.clear()
+            thread.start()
+            if len(threads) >= max_threads:
+                for t in threads:
+                    t.join()  # Wait for current threads to finish
+                threads = []  # Clear the list for the next batch
+        for t in threads:
+            t.join()
+        self.gui.logger(f"Generated {combinations_generator.total_processed} combinations.")
+        self.gui.logger(f"Data exported to '{combinations_generator.write_path}'.")
+        self.gui.run_button.configure(state=ctk.DISABLED)
+        return None
+
+class GUI:
+    def __init__(self):
+        # objects callable
+        self.error_handler = None
+        self.combinations_generator = None
+        self.file_handler = None
+
+        # libraries
+        self.error_messages = global_var.error_messages
+        self.UI_grid = global_var.UI_grid
         self.log = ""
 
         # UI main
@@ -210,47 +383,15 @@ class GUI:
         # main loop, only to the end pls
         self.root.mainloop()
 
-    def validate_file_path(self, file_path):
-        if not os.path.exists(file_path):
-            messagebox.showerror(title=self.error_messages["Invalid File Path"][0],
-                                 message=self.error_messages["Invalid File Path"][1])
-            self.logger(f"Invalid File Path: '{file_path}'")
-            self.run_button.configure(state=ctk.DISABLED)
-            return None
-        try:
-            header = None
-            if self.have_headers.get():
-                header = 'infer'
-            df_validated = pd.read_csv(file_path, dtype=str, header=header, encoding='utf-8')
-        except UnicodeDecodeError:
-            messagebox.showerror(title=self.error_messages["UnicodeDecodeError"][0],
-                                 message=self.error_messages["UnicodeDecodeError"][1])
-            self.logger(self.error_messages["UnicodeDecodeError"][1])
-            self.run_button.configure(state=ctk.DISABLED)
-            return
-        self.logger(f"File path: '{file_path}'")
-        self.run_button.configure(state=ctk.NORMAL)
-        self.entry_box.configure(state=ctk.DISABLED)
-        return df_validated
+    def set_file_handler(self, main_file_handler):
+        self.file_handler = main_file_handler
 
-    def clean_raw_df(self):
-        df_to_clean = pd.DataFrame()
-        try:  # Raise exception when df is empty.
-            df_to_clean = self.df_raw.apply(lambda col: col.dropna().reset_index(drop=True))
-            self.logger(f"Removed NaN rows.")
-            max_len = df_to_clean.apply(len).max()
-            df_to_clean = df_to_clean.apply(lambda col: col.reindex(range(max_len)))
-        except AttributeError:
-            messagebox.showerror(title=self.error_messages["AttributeError"][0],
-                                 message=self.error_messages["AttributeError"][1])
-        if self.have_name_and_code.get():
-            df_to_clean = self.combine_columns(df_to_clean)
-        self.df_cleaned = df_to_clean.replace('%%', np.nan, regex=False)
-        return
+    def set_combinations_generator(self, main_combinations_generator):
+        self.combinations_generator = main_combinations_generator
 
     def display_df(self, df_to_display=None):
         if df_to_display is None:
-            df_to_display = self.df_raw
+            df_to_display = self.file_handler.df_raw
         self.df_tree.delete(*self.df_tree.get_children())
         self.df_tree["columns"] = list(df_to_display.columns)
         self.df_tree["show"] = "headings"  # Hide the first empty column
@@ -274,126 +415,22 @@ class GUI:
         self.df_frame.grid_rowconfigure(0, weight=1)
         self.df_frame.grid_columnconfigure(0, weight=1)
 
-    def get_expected_output(self, df_to_read):
-        number_in_column = df_to_read.notna().sum()
-        expected_combinations = number_in_column.prod()
-        self.logger(f"{expected_combinations} rows expected.")
-        self.expected_output = expected_combinations
-        return expected_combinations
-
-    def limit_check(self, warning_limit = 1_000_000): # Raise exception when data to process is too large.
-        if self.get_expected_output(self.df_cleaned) > warning_limit:
-            self.logger(self.error_messages["LimitWarning"][2])
-            selected_button = messagebox.askquestion(title=self.error_messages["LimitWarning"][0],
-                                                     message=self.error_messages["LimitWarning"][1],
-                                                     icon=messagebox.WARNING, type=messagebox.OKCANCEL)
-            if selected_button == "cancel":
-                self.logger("User cancelled.")
-                return
-        return
-
-    @ staticmethod
-    def combine_columns(df_to_combine):
-        concatenated_columns = []
-        for i in range(0, len(df_to_combine.columns), 2):
-            # Concatenate two columns
-            if i + 1 < len(df_to_combine.columns):
-                combined = df_to_combine.iloc[:, i].fillna('') + '%%' + df_to_combine.iloc[:, i + 1].fillna('')
-            else:
-                combined = df_to_combine.iloc[:, i].fillna('')  # Handle odd number of columns
-            concatenated_columns.append(combined)
-        print(concatenated_columns)
-        df_combined = pd.concat(concatenated_columns, axis=1)
-        return df_combined
-
-    def split_columns(self, df_to_split):
-        result_df = pd.DataFrame()
-        try:
-            for col in df_to_split.columns:
-                split_cols = df_to_split[col].str.split('%%', expand=True)
-                split_cols.columns = [f"{col}_{i + 1}" for i in range(split_cols.shape[1])]
-                result_df = pd.concat([result_df, split_cols], axis=1)
-        except AttributeError:
-            messagebox.showerror(title=self.error_messages["RuntimeError"][0],
-                                 message=self.error_messages["RuntimeError"][1])
-            self.logger(self.error_messages["RuntimeError"][1])
-        return result_df
-
-    def chunk_generator(self, chunk_size):
-        print(self.df_cleaned)
-        generator = itertools.product(*[self.df_cleaned[col] for col in self.df_cleaned.columns])
-        chunk = []
-        for i, combination in enumerate(generator):
-            chunk.append(combination)
-            if len(chunk) == chunk_size:
-                yield chunk
-                chunk = []
-        if chunk:
-            yield chunk
-
-    def write_chunk(self, chunk, file_path, write_header):
-        df_output = pd.DataFrame(chunk, columns=self.df_cleaned.columns)
-        df_output.dropna(inplace=True)
-        if df_output.empty:
-            return
-        if self.have_name_and_code.get():
-            df_output = self.split_columns(df_output)
-            df_output.columns = self.df_raw.columns
-        # fixme chinese char breaks when writing from df to csv
-        file_lock = threading.Lock()
-        with file_lock:
-            df_output.to_csv(file_path, index=False, header=write_header, mode='a')
-        self.total_processed += df_output.shape[0]
-        percent_complete = round((self.total_processed / self.expected_output) * 100, 2)
-        self.log_label.configure(text=f"Loading... {percent_complete}% complete.")
-        if percent_complete == 100: # stop the main process cleanly
-            self.stop_event.set()
-            if self.process_thread.is_alive():
-                self.process_thread.join()
-        return
-
-    # fixme find out where the extra data coming from, and how to exit the main program when finished.
-    def thread_handler(self, chunk_size=100_000): # The actual main task that is called
-        file_path = self.get_new_file_path()
-        self.logger(f"Process started.")
-        self.total_processed = 0
-        max_threads = 4
-        threads = []
-        for i, chunk in enumerate(self.chunk_generator(chunk_size=chunk_size)):
-            print(f"chunk size: {len(chunk)}")
-            write_header = (i == 0)
-            thread = threading.Thread(target=self.write_chunk, args=(chunk, file_path, write_header), daemon=True)
-            threads.append(thread)
-            self.stop_event.clear()
-            thread.start()
-            print(f"Thread {i} started.")
-            if len(threads) >= max_threads:
-                for t in threads:
-                    t.join()  # Wait for current threads to finish
-                threads = []  # Clear the list for the next batch
-        for t in threads:
-            t.join()
-        self.logger(f"Generated {self.total_processed} combinations.")
-        self.logger(f"Data exported to '{file_path}'.")
-        self.run_button.configure(state=ctk.DISABLED)
-
     def main_task(self): # Basically other important functions other than main task.
         self.log = ""
-        self.clean_raw_df()
-        self.limit_check()
-        self.process_thread = threading.Thread(target=self.thread_handler, daemon=True) # Create new thread to handle back end processing.
-        self.process_thread.start()
+        self.file_handler.clean_raw_df()
+        expected_combinations, error_str = self.combinations_generator.limit_check(df=self.file_handler.df_cleaned)
+        if error_str:
+            self.logger(self.error_messages["LimitWarning"][2])
+            if error_str == "cancel":
+                return error_str
+        self.logger(f"{self.combinations_generator.expected_output} rows expected.")
+        self.combinations_generator.write_path = FileHandler.get_new_file_path(self.entry_box.get())
+        # Create new thread to handle back end processing.
+        process_thread = threading.Thread(target=self.combinations_generator.thread_handler, daemon=True)
+        process_thread.start()
+        return None
 
-    def get_new_file_path(self):
-        file_path = self.entry_box.get()
-        temp_path_list = file_path.split(".")
-        new_file_path = temp_path_list[0] + "_output" + ".csv"
-        while os.path.exists(new_file_path):  # fixme file names checked for duplicates not very profound
-            temp_path_list = new_file_path.split(".")
-            new_file_path = temp_path_list[0] + "(1)" + ".csv"
-        return new_file_path
-
-    def logger(self, log_str):
+    def logger(self, log_str: str):
         self.log_label.configure(text=log_str)
         print(log_str)
         self.log += "[" + datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "] " + log_str + "\n"
@@ -403,7 +440,7 @@ class GUI:
         self.entry_box.configure(state=ctk.NORMAL)
         self.entry_box.delete(0, ctk.END)
         self.entry_box.insert(0, file_path)
-        self.df_raw = self.validate_file_path(file_path)
+        self.file_handler.validate_file_path(file_path, self.have_headers.get())
         self.display_df()
         self.logger(f"CSV loaded: {file_path}")
 
@@ -418,4 +455,9 @@ class GUI:
         sys.exit()
 
 if __name__ == "__main__":
-    GUI()
+    main_gui = GUI()
+    error_handler = ErrorHandler(main_gui)
+    combinations_generator = CombinationsGenerator(main_gui)
+    file_handler = FileHandler(main_gui)
+    main_gui.set_file_handler(file_handler)
+    main_gui.set_combinations_generator(combinations_generator)
